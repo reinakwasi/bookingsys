65483b39e37d4676b356ec78defed780a5ae0a1a
Reindolf Appiagyei <reinapp734@gmail.com>
2025-10-24 03:59:23 +0000

    F

--- app/test-notifications/page.tsx
+++ app/test-notifications/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { toast } from 'react-hot-toast';
+import { toast } from 'sonner';
 
 export default function TestNotifications() {
   const [email, setEmail] = useState('');
